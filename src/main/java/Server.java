112a618199d9ee71e934937de0c5701ae337907b
Ildar Gilmutdinov <ir.gilmutdinov@mail.ru>
2022-10-27 22:36:07 +0400

    first commit

---
+++ src/main/java/Server.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(8084);) {
+            System.out.println("Сервер запущен!");
+            while (true)
+                try (Socket clientSocket = serverSocket.accept(); // ждем подключения
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                ) {
+
+                    System.out.println("New connection accepted");
+
+                    final String name = in.readLine();
+
+                    out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+
+
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
