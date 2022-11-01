7d7442227e8f0785c0c5e75e26db62707ae3e53e
Ildar Gilmutdinov <ir.gilmutdinov@mail.ru>
2022-11-01 20:34:54 +0400

    first commit

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -3,15 +3,13 @@ import java.net.Socket;
 
 public class Client {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         try (Socket clientSocket = new Socket("127.0.0.1", 8084);
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));// записываем данные
         ) {
-            out.println("Hello");
-            out.println("netology");
-            out.println("How are you?");
+            out.println();
             System.out.println(in.readLine());
 
         } catch (IOException e) {
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -12,14 +12,9 @@ public class Server {
                      PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
                      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 ) {
-
                     System.out.println("New connection accepted");
-
                     final String name = in.readLine();
-
                     out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
-
-
                 } catch (Exception e){
                     e.printStackTrace();
                 }
